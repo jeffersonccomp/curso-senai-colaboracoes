3bd6c096bfe2cec2878c75a8238a305fa7e8c9fc
JeffersonMoraes <jefferson_cc2012@hotmail.com>
2021-09-29 03:21:16 -0300

    cadastro pecas atualizacao 1

--- pecas.js
+++ pecas.js
@@ -1,21 +1,24 @@
-var listaDePecas = ["Mangueira Corta Fogo", "Bateria", "Filtro de Ar", "Filtro de Oleo", "Disco de Freio", "Farol Direito", "Sensor de Ré", "Amortecedor", "Retrovisor Direito"]
+var listaPecas = ["Mangueira Corta Fogo", "Bateria", "Filtro de Ar",
+    "Filtro de Oleo", "Farol Direito", "Amortecedor", "Pneus",
+    "Retrovisor", "Radio"
+]
 
-if (listaDePecas.length <= 10) {
+if (listaPecas.length <= 10) {
     console.log("É possivel cadastrar peças");
 
 } else {
     console.log("Não é possivel cadastrar mais peças");
 }
 
-let pesoPeca = 120;
+let pesoPeca = 200;
 
-if (pesoPeca < 100) {
-    console.log("A peça não possui o peso adequado");
-} else {
+if (pesoPeca > 100) {
     console.log("A peça possui o peso adequado");
+} else {
+    console.log("A peça não possui o peso adequado");
 }
 
-let nomePeca = ("Bateria");
+let nomePeca = ("Pneus");
 
 switch (nomePeca.length) {
 
